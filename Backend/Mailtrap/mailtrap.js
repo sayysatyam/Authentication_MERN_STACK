255e3cf10128ff98aba8349c7283b92f6f30c79e
SATYAM JHA <sayysatyam@gmail.com>
2026-01-26 06:03:13 +0530

    Update NodeMailer

--- Backend/Mailtrap/mailtrap.js
+++ Backend/Mailtrap/mailtrap.js
@@ -1,10 +1,14 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+
 const mailtrapClient = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Must be false for port 587
+  requireTLS: true, // Forces a secure connection
   auth: {
     user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    pass: process.env.EMAIL_PASS, // Must be a 16-character App Password
   },
 });
 
