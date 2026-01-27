f906f9d21e80bbe3431c278f2e56641cf5e6d096
SATYAM JHA <sayysatyam@gmail.com>
2026-01-27 16:45:43 +0530

    change theme

--- Frontend/mernStack/src/component/LoaderSpiner.jsx
+++ Frontend/mernStack/src/component/LoaderSpiner.jsx
@@ -1,10 +1,27 @@
 const LoadingSpinner = () => {
   return (
-    <div className="min-h-screen bg-linear-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-    
-      <div className="w-16 h-16 border-4 border-green-200 border-t-green-500 rounded-full animate-spin" />
+    <div className="
+      min-h-screen
+      flex items-center justify-center
+      relative overflow-hidden
+      bg-linear-to-br
+      from-gray-950
+      via-purple-950
+      to-gray-900
+    ">
+      <div
+        className="
+          w-16 h-16
+          border-4
+          border-purple-300/40
+          border-t-purple-600
+          rounded-full
+          animate-spin
+        "
+      />
     </div>
   );
 };
 
 export default LoadingSpinner;
+
